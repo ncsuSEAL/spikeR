35bfe2d6c284fbba459e75c883ee2200008763c6
ocsmit <osmith2@ncsu.edu>
2023-04-23 20:42:22 -0400

    Add tinytest support

---
+++ inst/tinytest/test_spiker.R
@@ -0,0 +1,26 @@
+library(spiker)
+
+# Placeholder with simple test
+expect_equal(1 + 1, 2)
+
+
+test_center_simple <- function() {
+    npoints <- 365 * 1
+    sig <- rep(1, npoints)
+    sig[200:201] <- 1000
+    return(SpikeCenter(sig, 7, 0.1, 0.5, 10))
+}
+expect_equal(test_center_simple(), c(200, 201))
+
+test_center_seasonal <- function() {
+    npoints <- 365 * 2
+    sig <- rep(1, npoints)
+    for (i in 1:npoints) {
+      sig[i] <-  sin(2 * pi * i / 365) * cos(2 * pi * i / 365)
+    }
+    sig[400:401] <- 1
+    sig[200:203] <- -10
+    return(SpikeCenter(sig, 7, 0.1, 0.2, 10))
+}
+
+expect_equal(test_center_seasonal(), c(400, 401))
