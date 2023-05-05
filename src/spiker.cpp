ab6a450d359a9489b41688af6f13a5db32229218
ocsmit <osmith2@ncsu.edu>
2023-05-05 08:29:30 -0400

    Formatting

--- src/spiker.cpp
+++ src/spiker.cpp
@@ -1,17 +1,17 @@
 /**
  * Author(s):    Owen Smith, Jenna Abrahamson
- * Created:   	 04.23.2023
+ * Created:      04.23.2023
  * License:      MIT
  **/
 #include "spiker.h"
 
 double populationSD(NumericVector &x1, NumericVector &x2)
 {
-	int n1, n2;
+    int n1, n2;
     double x, q1, q2;
 
-	n1 = x1.size();
-	n2 = x2.size();
+    n1 = x1.size();
+    n2 = x2.size();
 
     // Sample variances
     q1 = (n1 - 1) * var(x1) + n1 * pow(mean(x1), 2);
@@ -26,17 +26,19 @@ double populationSD(NumericVector &x1, NumericVector &x2)
 
 // [[Rcpp::export]]
 IntegerVector spikeCenter(
-	NumericVector signal,
-	int window,
-	double threshold,
-	double spikeAmp,
-	int timeframe,
-	Nullable<IntegerVector> dates_idx = R_NilValue
-) {
+    NumericVector signal,
+    int window,
+    double threshold,
+    double spikeAmp,
+    int timeframe,
+    Nullable<IntegerVector> dates_idx = R_NilValue
+    ) {
+    // TODO: Reduce number of allocs happening. Should pass signal by ref and
+    //       change to NA directly
 
-	int window_floor = floor(window / 2);
-	double center, pre_diff, post_diff;
-	NumericVector pre, post;
+    int window_floor = floor(window / 2);
+    double center, pre_diff, post_diff;
+    NumericVector pre, post;
     IntegerVector dates_idx_;
 
     // NOTE: Should move this logic into the R function
@@ -47,54 +49,46 @@ IntegerVector spikeCenter(
         dates_idx_ = dates_idx;
     }
 
-	dates_idx_ = dates_idx_[!is_na(signal)];
-	signal = na_omit(signal);
-	IntegerVector spikes(signal.size());
+    dates_idx_ = dates_idx_[!is_na(signal)];
+    signal = na_omit(signal);
+    IntegerVector spikes(signal.size());
 
+    if (signal.size() > window) {
+        // Loop through windows
+        for (int i = window_floor; i < (signal.size() - window_floor); ++i) {
+            // Get observation of interest and window before/after
+            center = signal[i];
+            pre = signal[Range(i - window_floor, i - 1)];
+            post = signal[Range(i + 1, i + window_floor)];
 
-	if (signal.size() > window) {
-		// Loop through windows
-		for (int i = window_floor; i < (signal.size() - window_floor); ++i) {
-			// Get observation of interest and window before/after
-			center = signal[i];
-			pre = signal[Range(i - window_floor, i - 1)];
-			post = signal[Range(i + 1, i + window_floor)];
+            // Get differences between median values before and after the 
+            // central observation
+            pre_diff = center - median(pre);
+            post_diff = median(post) - center;
 
-			// Get differences between median values before and after the 
-			// central observation
-			pre_diff = center - median(pre);
-			post_diff = median(post) - center;
+            if (
+                // If the differences before/after the central observation are
+                // greater than the threshold deviations between the median
+                // values pre- and post-observation of interest
+                (fabs(pre_diff + post_diff) >= 
+                    (threshold * populationSD(pre, post))
+                ) && 
+                // And the range of dates is within the timeframe threshold
+                ((dates_idx_[i + window_floor] - 
+                        dates_idx_[i - window_floor]) <= timeframe)
+                ) {
+                // If the difference before and after the central obs >= spike
+                // amplitude
+                if (
+                    ((pre_diff <= -spikeAmp) && (post_diff >= spikeAmp)) ||
+                    ((pre_diff >= spikeAmp) && (post_diff <= -spikeAmp))
+                    ) {
 
+                    spikes[i] = 1;
+            }
+        }
+    }
+}
 
-			if (
-			// If the differences before/after the central observation are
-			// greater than the threshold deviations between the median values 
-			// pre- and post-observation of interest
-				(
-					fabs(pre_diff + post_diff) >= 
-					(threshold * populationSD(pre, post))
-				) && 
-			// And the range of dates is within the timeframe threshold
-				(
-                    (dates_idx_[i + window_floor] - dates_idx_[i - window_floor]) <=
-					timeframe 
-				)
-				) {
-				// If the difference before and after the central obs >= spike
-				// amplitude
-				if (
-					((pre_diff <= -spikeAmp) && (post_diff >= spikeAmp)) ||
-					((pre_diff >= spikeAmp) && (post_diff <= -spikeAmp))
-				) {
-
-					spikes[i] = 1;
-				}
-
-			}
-
-		}
-
-	}
-
-	return dates_idx_[spikes == 1];
+return dates_idx_[spikes == 1];
 }
